f4e9b1eaa6ff9142a80b13f2b669b9021f1c13ba
JuTo_Hub <JustinTorres1@Outlook.com>
2020-10-18 15:41:20 -0500

    Added questioning criteria

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+const inquirer = require('inquirer');
+const licenses = {}
+
+const answers = inquirer.prompt([
+      {
+          type: "input",
+          message: "What is the title of your project?",
+          name: "title"
+      },
+      {
+        type: "input",
+        message: "Give a short description of your project",
+        name: "decription"
+    },
+    {
+        type: "input",
+        message: "Please give installation instructions.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Please explain the use of your application",
+        name:"usage"
+    },
+    {
+        type: "list",
+        message: "Please choose your license",
+        name: "license",
+        choices: licenses
+    },
+    {
+        type: "input",
+        message: "Please add the link to your github page",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    ])
   .then(answers => {
     // Use user feedback for... whatever!!
+    console.log(responses);
+    const filename = `${response.name}.md`
+    false.writeFile(filename, reponses, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Great. Thank You");
+        }
+    })
   })
   .catch(error => {
     if(error.isTtyError) {
